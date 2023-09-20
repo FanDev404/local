10fa36c4b58131a3786554248788ceb5c7edb0db
FanDev <fandev404@gmail.com>
2023-09-20 19:14:52 +0700

    404

---
+++ mariadb/entrypoint.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# TODO: rebuild docker images
+
+# Set environment variable that holds the Internal Docker IP
+INTERNAL_IP=$(ip route get 1 | awk '{print $(NF-2);exit}')
+export INTERNAL_IP
+
+# Switch to the container's working directory
+cd /home/container || exit 1
+
+# Convert all of the "{{VARIABLE}}" parts of the command into the expected shell
+# variable format of "${VARIABLE}" before evaluating the string and automatically
+# replacing the values.
+PARSED=$(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')
+
+# Display the command we're running in the output, and then execute it with the env
+# from the container itself.
+printf "\033[1m\033[33mcontainer@pterodactyl~ \033[0m%s\n" "$PARSED"
+
+# shellcheck disable=SC2086
+eval ${PARSED}
